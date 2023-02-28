b4a87739fbd6d15f6053a66300d8b48d463ea425
Aakashchaudhary0001 <aakashchaudhary1@pbpartners.com>
2023-02-28 18:33:58 +0530

    add product UPI

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,14 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-add-product.component.css']
 })
 export class SellerAddProductComponent implements OnInit {
+  addProductMessage: string | undefined;
 
-  constructor() { }
+
+  constructor(private product: ProductService) { }
 
   ngOnInit(): void {
   }
 
-  submit(data:object){
-    console.log(data);
-    
+  submit(data: product) {
+
+    this.product.addProduct(data).subscribe((result) => {
+      console.log(result);
+      if(result){
+        this.addProductMessage="Product is successfully added";
+
+      }
+      setTimeout(()=>(this.addProductMessage= undefined), 3000);
+    })
+
+
   }
 }
