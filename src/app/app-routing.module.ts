905a72395123505c28ae3a574ffa44ef775915e6
Aakashchaudhary0001 <aakashchaudhary1@pbpartners.com>
2023-03-11 19:52:26 +0530

    product detaiils page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { SearchComponent } from './search/search.component';
 import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
@@ -39,6 +40,10 @@ const routes: Routes = [
   {
     path: 'search/:query',
     component: SearchComponent,
+  },
+  {
+    path: 'details/:productId',
+    component: ProductDetailsComponent,
   },
 
 
